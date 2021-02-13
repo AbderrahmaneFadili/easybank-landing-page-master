44b1e4a081a56ab43d98aa394fc126dcf8674091
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-02-13 13:35:03 +0100

    Finishing the Work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("EasyBank");
+/* /////////////  Open & Close Menu //////////// */
+const body = document.querySelector("body");
+const navToggle = document.querySelector(".nav-toggle");
+const overlay = document.querySelector(".overlay");
+
+navToggle.addEventListener("click", () => {
+  body.classList.toggle("open");
+});
+
+overlay.addEventListener("click", () => {
+  body.classList.remove("open");
+});
